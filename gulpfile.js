4e6bbbb76b1ab8adae4853e0a21bb930a26cf225
Andrew Clark <drewboy879@gmail.com>
2023-05-23 00:46:17 -0500

    gulp is working a little, I think

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+
+// Define the 'styles' task as an asynchronous function
+gulp.task('styles', function stylesTask(done) {
+    gulp.src('./sass/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(gulp.dest('./css'));
+
+    done();
+});
+
+// Define the 'watch' task
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.scss', gulp.series('styles'));
+});
