a9eb93ac051a38d4a239fcff020435a35fe18aa9
Andrew Clark <drewboy879@gmail.com>
2023-05-23 01:58:44 -0500

    Refactor gulpfile.js to remove deprecated syntax
    This commit removes the use of the deprecated 'gulp.task' syntax and replaces it with the new function-based approach. The 'styles' and 'watch' tasks have been updated accordingly.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 
-// Define the 'styles' task as an asynchronous function
+
 gulp.task('styles', function stylesTask(done) {
     gulp.src('./sass/main.scss')
         .pipe(sass().on('error', sass.logError))
@@ -14,7 +14,7 @@ gulp.task('styles', function stylesTask(done) {
     done();
 });
 
-// Define the 'watch' task
+
 gulp.task('watch', function() {
     gulp.watch('sass/**/*.scss', gulp.series('styles'));
 });
