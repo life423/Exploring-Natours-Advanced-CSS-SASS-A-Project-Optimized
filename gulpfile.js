864bfe20992dfed88b58a3eae59d89f4b0b77e42
Andrew Clark <drew@drewclark.io>
2023-07-06 15:44:20 -0500

    Refactor gulpfile.js, optimize image compression
    - Refactored the import statements in gulpfile.js to use double quotes instead of single quotes for consistency.
    - Updated the destination folder for the images task from "./img" to "./img-min".
    - Added a new task "watch" to watch for changes in Sass files and trigger the "styles" task.
    - Created new optimized versions of several image files and moved them to the "./img-min" folder.
    - Deleted the old favicon.ico file and added a new favicon.png file.
    
    These changes improve code readability and optimize image compression.

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
-import gulp from 'gulp';
-import sassCompiler from 'sass';
-import gulpSass from 'gulp-sass';
-import autoprefixer from 'gulp-autoprefixer';
-import imagemin from 'gulp-imagemin';
+import gulp from "gulp";
+import sassCompiler from "sass";
+import gulpSass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import imagemin from "gulp-imagemin";
 
 const sass = gulpSass(sassCompiler);
 
-gulp.task('styles', function stylesTask(done) {
-    gulp.src('./sass/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            cascade: false
-        }))
-        .pipe(gulp.dest('./css'));
+gulp.task("styles", function stylesTask(done) {
+  gulp
+    .src("./sass/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest("./css"));
 
-    done();
+  done();
 });
 
-gulp.task('images', function(done) {
-    gulp.src('./img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./img'));
-
-    done();
+gulp.task("images", function (done) {
+  gulp.src("./img/*").pipe(imagemin()).pipe(gulp.dest("./img-min"));
+  done();
 });
 
-gulp.task('watch', function() {
-    gulp.watch('sass/**/*.scss', gulp.series('styles'));
-    gulp.watch('img/*', gulp.series('images'));
+gulp.task("watch", function () {
+  gulp.watch("sass/**/*.scss", gulp.series("styles"));
+  
 });
